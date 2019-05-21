be93c330746f4cc91ba7ccc5ea9ebf3d54890943
Dan Hislop <40676685+danhislop@users.noreply.github.com>
2019-05-20 18:35:53 -0700

    Now looping through 10 seasons of data
    had to reconfigure a lot to account for adding 'season' column.  
    next: normalizing was not working (is it needed?)
    look at correlations and conclusions
    can the double loop be sped up?

--- EPL.py
+++ EPL.py
@@ -7,30 +7,55 @@ Created on Fri Apr  12 19:41:50 2019
 pd.set_option('display.width', 700)
 pd.set_option('display.max_columns',21)
 """
-# Goal: summarize English Premier Leagues's 17-18 season.  Separate home v away record to look for correlations. 
-# Data source contains game results, but does not contain team win/loss records, need to calculate
+# Goal: summarize English Premier Leagues's statistis.  Separate home v away record to look for correlations. 
 
+### dan: you are adding season column into dataframe and now need to see it all the way through
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn import preprocessing
 from scipy import stats
 pd.set_option('display.width', 700)
 pd.set_option('display.max_columns',21)
 
-## Use the pandas read_csv method to read the file into a dataframe
-filepath = '/Users/danhi/Dropbox/bwork/python/datasets/season-1718_premier_league.csv'
-epl = pd.read_csv(filepath, index_col=0)
 
+def get_csv(scope):
+    ''' input 'scope' is a string indicating 'single' or 'all' seasons
+    purpose: use pandas read_csv to pull data from local files into dataframe
+    output: dataframe with specified seasons for all teams
+    '''
 
-def calc_record(team_df, home_or_away):
+    if scope == 'single':        
+        # SINGLE SEASON Use the pandas read_csv method to read the file into a dataframe
+        filepath = '/Users/danhi/Dropbox/bwork/python/datasets/season-1718_premier_league.csv'
+        epl = pd.read_csv(filepath, index_col=0)
+        epl['Season'] = '2017-18'
+    
+    elif scope == 'all':        
+        ## ALL SEASONS Use the pandas read_csv method to read the file into a dataframe
+        for i in range(10,20):
+            if i == 10:
+                filepath = '/Users/danhi/Dropbox/bwork/python/datasets/epl_data/season-09{}_csv.csv'.format(i)
+                epl = pd.read_csv(filepath, index_col=0)
+                epl['Season'] = '2009-10'
+            else:
+                filepath = '/Users/danhi/Dropbox/bwork/python/datasets/epl_data/season-{}{}_csv.csv'.format(i-1,i)
+                this_epl = pd.read_csv(filepath, index_col=0)
+                this_epl['Season'] = '20{}-{}'.format(i-1,i)
+                epl = epl.append(this_epl)
+    else:
+        print("please specify single or all")
+        return()
+        
+    return(epl)
+
+def calc_record(df, home_or_away):
 
     ''' 
     input: 
-        team_df = dataframe containing individual game results for this team's home (or away) games
-        home_or_away = string, denoting whether team_df contains games played at home, or away
+        df = dataframe containing individual game results for this team's home (or away) games
+        home_or_away = string, denoting whether df contains games played at home, or away
     purpose: calculate (Wins, Draws, Loss, Points) record for the games passed in
         note: Because dataset designates winner as 'home' or 'away' team, we need to separate in order to calculate
     output: returns a list of (Wins, Draws, Loss, Points) for this team's home or away game
@@ -38,17 +63,17 @@ def calc_record(team_df, home_or_away):
     
     if home_or_away == 'home':
         record = [
-                team_df.loc[(team_df['FTR'] == 'H')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'D')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'A')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'H')].shape[0]*3 + team_df.loc[(team_df['FTR'] == 'D')].shape[0]*1,]
+                df.loc[(df['FTR'] == 'H')].shape[0],
+                df.loc[(df['FTR'] == 'D')].shape[0],
+                df.loc[(df['FTR'] == 'A')].shape[0],
+                df.loc[(df['FTR'] == 'H')].shape[0]*3 + df.loc[(df['FTR'] == 'D')].shape[0]*1,]
 
     elif home_or_away == 'away':
         record = [
-                team_df.loc[(team_df['FTR'] == 'A')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'D')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'H')].shape[0],
-                team_df.loc[(team_df['FTR'] == 'A')].shape[0]*3 + team_df.loc[(team_df['FTR'] == 'D')].shape[0]*1,]
+                df.loc[(df['FTR'] == 'A')].shape[0],
+                df.loc[(df['FTR'] == 'D')].shape[0],
+                df.loc[(df['FTR'] == 'H')].shape[0],
+                df.loc[(df['FTR'] == 'A')].shape[0]*3 + df.loc[(df['FTR'] == 'D')].shape[0]*1,]
     else:
         print("please specify home or away")
         return()
@@ -56,20 +81,20 @@ def calc_record(team_df, home_or_away):
     return(record)
     
     
-def calc_stats(team_df, home_or_away):
+def calc_stats(df, home_or_away):
 
     ''' 
     input: 
-        team_df = dataframe containing individual game results for this team's home (or away) games
-        home_or_away = string, denoting whether team_df contains games played at home or away
+        df = dataframe containing individual game results for this team's home (or away) games
+        home_or_away = string, denoting whether df contains games played at home or away
     purpose: tabulate stats (Corner Kicks, Fouls, etc) for the games passed in
         note: Because dataset designates stats as 'home' or 'away' team, we need to separate in order to calculate
     output: returns a dataframe of (Shots, Shots on Target, Fouls Committed, Fouls Awarded (based on other team's fouls committed), Corners, Yellows, and Red Cards) for this team's home or away game
     '''
     
     if home_or_away == 'home':
-        stats = team_df[['HomeTeam','AwayTeam','FTR','HS','HST','HF','AF','HC','HY','HR']]
-        stats.columns = ['Team', 'Opponent', 'FTR', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards']
+        stats = df[['Season','HomeTeam','AwayTeam','FTR','HS','HST','HF','AF','HC','HY','HR','FTHG']]
+        stats.columns = ['Season','Team', 'Opponent', 'FTR', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards', 'GoalsScored']
         stats = stats.assign(Location = 'Home')
         # Keep track of who won via points earned
         stats.loc[stats.FTR=='H', 'FTR'] = 3
@@ -78,8 +103,8 @@ def calc_stats(team_df, home_or_away):
 
 
     elif home_or_away == 'away':
-        stats = team_df[['AwayTeam','HomeTeam','FTR','AS','AST','AF','HF','AC','AY','AR']]
-        stats.columns = ['Team', 'Opponent', 'FTR', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards']
+        stats = df[['Season','AwayTeam','HomeTeam','FTR','AS','AST','AF','HF','AC','AY','AR', 'FTAG']]
+        stats.columns = ['Season','Team', 'Opponent', 'FTR', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards', 'GoalsScored']
         stats = stats.assign(Location = 'Away')
         # Keep track of who won via points earned
         stats.loc[stats.FTR=='H', 'FTR'] = 0
@@ -91,8 +116,7 @@ def calc_stats(team_df, home_or_away):
         return()
     
     stats.rename(columns={'FTR':'PointsEarned'}, inplace=True)  
-    return(stats)
-    
+    return(stats)   
 
 def team_df(teamname, year, df):
     '''
@@ -101,14 +125,14 @@ def team_df(teamname, year, df):
         year = string, e.g. 2017-18
         df = dataframe containing individual game results for all teams over whole season
     purpose: drop all games $teamname wasn't in,then calculate home, away, and overall record
-    output: a list containing teamname, year, season record, home record, away record 
+    output: record_output is a list containing teamname, year, season record, home record, away record 
+    output: allstats is a dataframe with all stats for that season
     '''
     info = [teamname,year]
     
     # create dataframes for each team: home games, away games
-    #all = epl.loc[(epl['HomeTeam'] == teamname) | (epl['AwayTeam'] == teamname), :]
-    home = epl.loc[(epl['HomeTeam'] == teamname)]
-    away = epl.loc[(epl['AwayTeam'] == teamname)]
+    home = df.loc[(df['HomeTeam'] == teamname)]
+    away = df.loc[(df['AwayTeam'] == teamname)]
 
 
     # homerecord looks like this: (13, 4, 2, 43) (homewins, homedraws, homeloss, homepoints)
@@ -116,12 +140,12 @@ def team_df(teamname, year, df):
     homerecord = calc_record(home, 'home')    
     awayrecord = calc_record(away, 'away')   
     
-    # calculate the stats now
+    # calculate the stats
     homestats = calc_stats(home, 'home')
     awaystats = calc_stats(away, 'away')
     #combine stats home,away dataframes into one allstats dataframe
     frames = [homestats, awaystats]
-    allstats = pd.concat(frames)
+    allstats = pd.concat(frames) # now will allstats need the 'season' info?
     
     # add home and away records to create season record
     seasonrecord = []
@@ -129,62 +153,48 @@ def team_df(teamname, year, df):
         seasonrecord.append(homerecord[i] + awayrecord[i])
     
     # return all above info in a list     
-    output = []
-    output.extend(info + seasonrecord + homerecord + awayrecord)    
+    record_output = []
+    record_output.extend(info + seasonrecord + homerecord + awayrecord)    
     #return(output)
-    return(allstats, output)
+    return(allstats, record_output)
+    
+def clean(seasonstats_here):
+    '''input: seasonstats dataframe from any season
+    purpose: clean up by performing the steps below
+    output: seasonstats dataframe
+    '''       
 
-year = '2017-18'
-teamnames = epl.HomeTeam.unique()
-teamnames = np.sort(teamnames, axis=0)
-
-# Create dataframe to store all team records
-record_cols = ['Team', 'Season', 'Win', 'Draw', 'Loss', 'Points', 'HomeWin', 'HomeDraw', 'HomeLoss', 'HomePoints', 'AwayWin', 'AwayDraw', 'AwayLoss', 'AwayPoints']
-team_records = pd.DataFrame(columns = record_cols)
-
-stats_cols = ['Team', 'Opponent', 'Location','PointsEarned', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards']
-seasonstats = pd.DataFrame(columns = stats_cols)
-
-#for name in ['Tottenham', 'Chelsea']:
-for name in teamnames:
-    stat, out = team_df(name, year, epl)
-
-    # append each list into a new DF.  Using len(team_records) to determine next empty row
-    team_records.loc[len(team_records)] = out
-
-    #combine all team stats dataframes into one all season dataframe
-    seasonstats = seasonstats.append(stat)
-
-
-# SeasonStats: reset the column order; through the append process it gets changed
-seasonstats = seasonstats[stats_cols]
-
-''' At this point we have SeasonStats for all teams for one season.  Now to normalize, sort, and reindex '''
-
-# Move gamedate to column, then reindex
-seasonstats.reset_index(level=0, inplace=True)
-#seasonstats['GameDate'] = seasonstats.index
-seasonstats.rename(columns={'index':'GameDate'}, inplace=True)
-
-# Records: index by team and sort by most points
-team_records.set_index("Team", inplace=True)
-team_records.sort_values(by='Points', ascending=False, inplace=True)
-
-# Change numerical columns to float, in preparation for finding correlation 
-for i in range(4,len(seasonstats.columns)):
-    seasonstats[seasonstats.columns[i]] = seasonstats[seasonstats.columns[i]].astype('float64')
-
-# one hot encoding for location, a potentially important factor in correlation
-seasonstats = pd.concat([seasonstats, pd.get_dummies(seasonstats['Location'], prefix='Location')], axis=1)
-seasonstats.drop(['Location'], axis = 1, inplace=True)
+    # SeasonStats: reset the column order; through the append process it gets changed
+    seasonstats_here = seasonstats_here[stats_cols]
+    
+    # Move gamedate to column, then reindex
+    seasonstats_here.reset_index(level=0, inplace=True)
+    #seasonstats_here['GameDate'] = seasonstats_here.index
+    seasonstats_here.rename(columns={'index':'GameDate'}, inplace=True)
+    
+    # Records: index by team and sort by most points
+    #team_records.set_index('Team', inplace=True)
+    #team_records.sort_values(by='Points', ascending=False, inplace=True)
+    
+    # Change numerical columns to float, in preparation for finding correlation 
+    for i in range(5,len(seasonstats_here.columns)):
+        seasonstats_here[seasonstats_here.columns[i]] = seasonstats_here[seasonstats_here.columns[i]].astype('float64')
+    
+    return(seasonstats_here)
 
 def normalize(seasonstats_df):
     ''' input: seasonstats dataframe.  this function makes assumptions about index values
     purpose: Normalize the numeric values of the dataframe
     output: dataframe with all numeric values replaced by normalized values
     '''
+    
+    #### DAN TO TEST OHE here    
+    # one hot encoding for location, a potentially important factor in correlation
+#    seasonstats_df = pd.concat([seasonstats_df, pd.get_dummies(seasonstats_df['Location'], prefix='Location')], axis=1)
+#    seasonstats_df.drop(['Location'], axis = 1, inplace=True)
+    
     # separate out numeric columns to be normalized (makes assumptions about shape of seasonstats df)
-    numeric_columns = seasonstats_df.iloc[:,3:14]
+    numeric_columns = seasonstats_df.iloc[:,5:15]
     
     # normalize them
     min_max_scaler = preprocessing.MinMaxScaler()
@@ -192,38 +202,90 @@ def normalize(seasonstats_df):
     df_normalized = pd.DataFrame(np_scaled)
     
     #put back together with first 3 columns of seasonstats
-    normstats = pd.concat([seasonstats_df.iloc[:,0:3], df_normalized], axis=1, sort=False)
-    normstats_cols = ['GameDate', 'Team', 'Opponent', 'PointsEarned', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards', 'Location_Away' , 'Location_Home']
+    normstats = pd.concat([seasonstats_df.iloc[:,0:5], df_normalized], axis=1, sort=False)
+    normstats_cols = ['GameDate', 'Team', 'Opponent', 'PointsEarned', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards', 'GoalsScored', 'Location_Away' , 'Location_Home']
     normstats.columns = normstats_cols
     return(normstats)
-    
-# Normalize the Values in prep for correlations
-raw_seasonstats = seasonstats # saving this for future use with raw numbers
-seasonstats = normalize(seasonstats)
 
-''' At this point seasonstats is normalized, sorted, reindexed, and ready to process correlations '''
-# Taking an export here for JUPYTER NOTEBOOKS or R to evaluate:
-seasonstats.to_csv('seasonstats_normalized.csv', index = True)
-# Continue on to evaluate in this script
 
-correlations = seasonstats.corr(method='pearson')
-print(correlations)
+# Create empty dataframe to store all team records
+record_cols = ['Team', 'Season', 'Win', 'Draw', 'Loss', 'Points', 'HomeWin', 'HomeDraw', 'HomeLoss', 'HomePoints', 'AwayWin', 'AwayDraw', 'AwayLoss', 'AwayPoints']
+team_records = pd.DataFrame(columns = record_cols)
 
-pearson_coef, p_value = stats.pearsonr(seasonstats['PointsEarned'], seasonstats['Shots_on_Target'])
-print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value) 
+stats_cols = ['Team', 'Season', 'Opponent', 'Location','PointsEarned', 'Shots', 'Shots_on_Target', 'FoulsCommitted', 'FoulsAwarded', 'Corners', 'YellowCards', 'RedCards', 'GoalsScored']
+seasonstats = pd.DataFrame(columns = stats_cols)
 
-#####  DAN: maybe style doesn't work in spyder
-''' Steps:
-    0. normalize data?!
-1. show overall correlation matrix.  must be formated correctly
-2. drill in on important ones: SOT, shots, location
-3. Find and explain pearson_coef and P-value
-4. expand to more seasons.  track the winner from each season to see if different correlations
+# Read in the csv file inputs.  specify 'single' or 'all' for how many seasons to get
+epl = get_csv('all')
+
+seasons = {} # creates a dict to store each season's dataframe
+#campaigns = ['2017-18','2018-19']
+campaigns = epl.Season.unique().tolist()
+
+for c in campaigns:
+        # df with all of this season's games as input
+        this_df = epl.loc[(epl['Season'] == c)]  
+        
+        # dict: key is this season, later to add value of this season's stats
+        seasons[c] = pd.DataFrame(columns = stats_cols)        
+        print('processing Season: ', c)
+        
+        # Process each team's record:
+        teamnames = this_df.HomeTeam.unique()
+        #teamnames = ['Tottenham', 'Chelsea']
+        teamnames = np.sort(teamnames, axis=0)
+        print('teamnames for season',c,' :',teamnames)
+        for name in teamnames:
+            print('processing team:',name, 'for Season: ', c)
+            stat, record = team_df(name, c, this_df)
+   
+            # append each list into a new DF.  Using len(team_records) to determine next empty row
+            team_records.loc[len(team_records)] = record
+            
+            # for this season, combine all team stats dataframes into one season dataframe.
+            stat = clean(stat) 
+            seasons[c] = seasons[c].append(stat)[stat.columns.tolist()] # keeps column order
+
+# Now we can refer to any season's data set as e.g. allseasons['2017-18']
+# We also want to make a single dataset for all time:
+          
+allseasons = pd.DataFrame(columns = stat.columns.tolist())
+for key, value in seasons.items():
+    allseasons = allseasons.append(value)
+print(allseasons.shape)
+
+# Normalize the allseasons  - troubleshoot this next
+#allseasons = normalize(allseasons)
+
+
+
+#''' At this point we have SeasonStats for all teams for one season.  Now to normalize, sort, and reindex '''
+#raw_seasonstats = seasonstats # saving this for future use with raw numbers
+#seasonstats = clean(seasonstats)
+#seasonstats = normalize(seasonstats)
+
+
+
+
+
+
+
+
+''' Now seasonstats is normalized, sorted, reindexed, and ready to process correlations '''
+seasonstats.to_csv('seasonstats_normalized.csv', index = True) #export to evaluate in R or Jupyter
+
+correlations = allseasons.corr(method='pearson')
+print(correlations['PointsEarned'])
+
+pearson_coef, p_value = stats.pearsonr(allseasons['PointsEarned'], allseasons['Shots_on_Target'])
+print("The Pearson Correlation Coefficient for PointsEarned to Shots_on_Target is",
+      pearson_coef, " with a P-value of P =", p_value) 
+
+
+
 '''
-    
-    
 
-'''    PLOTTING the correlations '''
+#   PLOTTING the correlations 
 simple_correlations = seasonstats.corr()['PointsEarned'].sort_values(ascending=True)
 simple_correlations.drop(['PointsEarned'], axis=0, inplace=True)  # Drop the redundant self-correlation
 simple_correlations.plot(kind='barh', 
@@ -243,7 +305,17 @@ sot_correlations.plot(kind='barh',
                          xlim=(-1,1))
 
 plt.title('Correlation with Shots_on_Target', fontsize=15)
-plt.xticks=scale
+plt.show()
+
+goals_correlations = seasonstats.corr()['GoalsScored'].sort_values(ascending=True)
+goals_correlations.drop(['GoalsScored'], axis=0, inplace=True)  # Drop the redundant self-correlation
+goals_correlations.plot(kind='barh', 
+                         figsize=(10,5),
+                         color= 'green',
+                         fontsize = 12,
+                         xlim=(-1,1))
+
+plt.title('Correlation with GoalsScored', fontsize=15)
 plt.show()
 
 
@@ -279,7 +351,7 @@ lm.fit(X,Y)
 yhat = lm.predict(X)
 print('intercept is ', lm.intercept_, 'slope is ', lm.coef_)
 print('our equation is Y-hat = ',lm.intercept_, '+', lm.coef_, '* Shots on Target')
-
+'''
 # scatter matrix
 #from pandas.plotting import scatter_matrix
 #pd.plotting.scatter_matrix(correlations, figsize=(12,12))
